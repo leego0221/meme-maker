f550c747a5389bdce1e665937b2f963b40eddf2b
leego0221 <leego0221@naver.com>
2023-05-18 17:32:58 +0900

    painting board

--- app.js
+++ app.js
@@ -1,34 +1,98 @@
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+// Array.from()은 원래 배열이 아닌 컬렉션을 배열로 바꿔주는 메소드인 듯
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
-ctx.lineWidth = 2;
 
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-  "#7efff5",
-];
-let x = 0;
-let y = 0;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 
-function changePoint(event) {
-  x = event.offsetX;
-  y = event.offsetY;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
+let isFilling = false;
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return; // 불필요한 ctx.moveTo()의 실행을 막기 위해서
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-function drawLine(event) {
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+function startPainting() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
+  ctx.beginPath(); // 중요!!! 그리기 취소는 모든 행동의 끝이니 여기에 삽입
 }
 
-canvas.addEventListener("click", changePoint);
-canvas.addEventListener("mousemove", drawLine);
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+function changeColor(currentColor) {
+  ctx.strokeStyle = currentColor;
+  ctx.fillStyle = currentColor;
+}
+
+function onColorChange(event) {
+  changeColor(event.target.value);
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color; // html의 data-*를 활용하여 color를 가져온 것
+  changeColor(colorValue);
+  color.value = colorValue; // 선택한 색깔을 알려주기 위함
+}
+
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+
+function onDestroyClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
